fb789dd9aa7e739838d3cd824bf9784b039fec62
sergey <petrelevich@yandex.ru>
2019-11-16 00:56:54 +0300

    l12 behavioralPatterns

---
+++ L12-behavioralPatterns/src/main/java/ru/outs/state/StateProvider.java
@@ -0,0 +1,14 @@
+package ru.outs.state;
+
+public class StateProvider {
+  private static final State onState = new OnState();
+  private static final State offState = new OffState();
+
+  public static State getOnState() {
+    return onState;
+  }
+
+  public static State getOffState() {
+    return offState;
+  }
+}
