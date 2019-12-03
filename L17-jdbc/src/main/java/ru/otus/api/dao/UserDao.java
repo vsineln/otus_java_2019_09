371f7d639f79c6e75e47703abe3b8f5a62ece562
sergey <petrelevich@yandex.ru>
2019-12-04 00:37:33 +0300

    L17 jdbc

---
+++ L17-jdbc/src/main/java/ru/otus/api/dao/UserDao.java
@@ -0,0 +1,14 @@
+package ru.otus.api.dao;
+
+import java.util.Optional;
+
+import ru.otus.api.model.User;
+import ru.otus.api.sessionmanager.SessionManager;
+
+public interface UserDao {
+  Optional<User> findById(long id);
+
+  long saveUser(User user);
+
+  SessionManager getSessionManager();
+}
