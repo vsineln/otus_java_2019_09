4eacb6d47a88a73647aeb33505ac473293127358
Victoria Sinelnikova <vsineln@gmail.com>
2020-02-04 21:43:38 +0300

    AppComponentsContainer implementation

--- DIhwDraft/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ DIhwDraft/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,9 +1,19 @@
 package ru.otus.appcontainer;
 
+import org.apache.commons.lang3.tuple.Pair;
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
-import java.util.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
 
@@ -16,7 +26,25 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        // You code here...
+        try {
+            Object instance = configClass.getDeclaredConstructor().newInstance();
+            List<Pair<Integer, Method>> list = getMethodsOrdered(configClass.getMethods());
+            for (Pair<Integer, Method> pair : list) {
+                Method m = pair.getRight();
+                Class<?>[] params = m.getParameterTypes();
+                Object o;
+                if (params.length == 0) {
+                    o = m.invoke(instance);
+                } else {
+                    Object[] paramComponent = Arrays.stream(params).map(this::getAppComponent).toArray();
+                    o = m.invoke(instance, paramComponent);
+                }
+                appComponents.add(o);
+                appComponentsByName.put(getComponentName(m), o);
+            }
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -25,13 +53,27 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         }
     }
 
+    private List<Pair<Integer, Method>> getMethodsOrdered(Method[] methods) {
+        return Arrays.stream(methods).filter(method -> method.getAnnotation(AppComponent.class) != null)
+            .map(method -> Pair.of(method.getAnnotation(AppComponent.class).order(), method))
+            .sorted(Comparator.comparingInt(Pair::getLeft))
+            .collect(Collectors.toList());
+    }
+
+    private String getComponentName(Method m) {
+        AppComponent annotation = m.getAnnotation(AppComponent.class);
+        return annotation.name();
+    }
+
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        return (C) appComponents.stream()
+            .filter(component -> componentClass.isAssignableFrom(component.getClass()))
+            .findAny().orElseThrow(() -> new RuntimeException("Component is not found"));
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) appComponentsByName.get(componentName);
     }
 }
